8142688f4b27dba53d036ddc0e31a0ce2d42486a
Mohan Krishna <mohankrishnabattala@gmail.com>
2026-03-20 00:15:16 +0530

    Initial Spring Boot setup

---
+++ task-workflow-system/src/main/java/com/mohan/taskmanager/task_workflow_system/TaskWorkflowSystemApplication.java
@@ -0,0 +1,14 @@
+package com.mohan.taskmanager.task_workflow_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskWorkflowSystemApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Starting Task Workflow System Application...");
+		SpringApplication.run(TaskWorkflowSystemApplication.class, args);
+	}
+
+}
